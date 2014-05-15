42968d50117fca914ea93bf3cbc1261ea64e8fd1
Nikolas Wise <nikolaswise@gmail.com>
2014-05-15 15:01:13 -0700

    stub out views with sample code from dimplejs.org

--- examples/js/app.js
+++ examples/js/app.js
@@ -14,10 +14,29 @@ angular.module('myApp', [
     templateUrl: 'partials/line-graph.html',
     controller: 'lineGraphController'
   })
-
   .when('/stacked-area', {
     templateUrl: 'partials/stacked-area.html',
     controller: 'stackedAreaController'
+  })
+  .when('/area', {
+    templateUrl: 'partials/area.html',
+    controller: 'areaController'
+  })
+  .when('/expanded-stacked-area', {
+    templateUrl: 'partials/expanded-stacked-area.html',
+    controller: 'expandedAreaController'
+  })
+  .when('/bar-graph', {
+    templateUrl: 'partials/bar-graph.html',
+    controller: 'barController'
+  })
+  .when('/stacked-bar-graph', {
+    templateUrl: 'partials/stacked-bar-graph.html',
+    controller: 'stackedBarController'
+  })
+  .when('/scatter-plot', {
+    templateUrl: 'partials/scatter-plot.html',
+    controller: 'scatterController'
   })
 
   .otherwise({
