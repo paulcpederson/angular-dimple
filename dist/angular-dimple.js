dd2d02d43217b5f5914a08ff7e4c16b79af825fa
Nikolas Wise <nikolaswise@gmail.com>
2014-05-20 16:20:03 -0700

    legendary

--- dist/angular-dimple.js
+++ dist/angular-dimple.js
@@ -170,25 +170,36 @@ angular.module('angular-dimple.graph', [])
     }]
   };
 }]);
-angular.module('angular-dimple', [
-  'angular-dimple.core',
-  'angular-dimple.graph',
-  'angular-dimple.legend',
-  'angular-dimple.x',
-  'angular-dimple.y',
-  'angular-dimple.line',
-  'angular-dimple.bar',
-  'angular-dimple.stacked-bar',
-  'angular-dimple.area',
-  'angular-dimple.stacked-area',
-  'angular-dimple.scatter-plot'
-])
+angular.module('angular-dimple.legend', [])
 
-.constant('MODULE_VERSION', '0.0.1')
+.directive('legend', [function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    require: ['legend', '^graph'],
+    controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+    }],
+    link: function($scope, $element, $attrs, $controllers) {
+      var graphController = $controllers[1];
+      var chart = graphController.getChart();
 
-.value('defaults', {
-  foo: 'bar'
-});
+      function addLegend () {
+        var left = $attrs.left ? $attrs.left : "10%";
+        var top = $attrs.top ? $attrs.top : "4%";
+        var height = $attrs.height ? $attrs.height : "10%";
+        var width = $attrs.width ? $attrs.width : "90%";
+        var position = $attrs.position ? $attrs.position : 'left';
+        chart.addLegend(left, top, width, height, position);
+      }
+
+      $scope.$watch('data', function(newValue, oldValue) {
+        if (newValue) {
+          addLegend();
+        }
+      });
+    }
+  };
+}]);
 angular.module('angular-dimple.line', [])
 
 .directive('line', ['angular-dimple.core', function (core) {
