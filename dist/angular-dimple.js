0cc605c3227be87047c0d51fdaacf5581a8fe799
Nikolas Wise <nikolaswise@gmail.com>
2014-05-20 15:44:33 -0700

    no more xamples

--- dist/angular-dimple.js
+++ dist/angular-dimple.js
@@ -29,6 +29,7 @@ angular.module('angular-dimple.core', [])
 angular.module('angular-dimple', [
   'angular-dimple.core',
   'angular-dimple.graph',
+  'angular-dimple.legend',
   'angular-dimple.x',
   'angular-dimple.y',
   'angular-dimple.line',
@@ -169,6 +170,25 @@ angular.module('angular-dimple.graph', [])
     }]
   };
 }]);
+angular.module('angular-dimple', [
+  'angular-dimple.core',
+  'angular-dimple.graph',
+  'angular-dimple.legend',
+  'angular-dimple.x',
+  'angular-dimple.y',
+  'angular-dimple.line',
+  'angular-dimple.bar',
+  'angular-dimple.stacked-bar',
+  'angular-dimple.area',
+  'angular-dimple.stacked-area',
+  'angular-dimple.scatter-plot'
+])
+
+.constant('MODULE_VERSION', '0.0.1')
+
+.value('defaults', {
+  foo: 'bar'
+});
 angular.module('angular-dimple.line', [])
 
 .directive('line', ['angular-dimple.core', function (core) {
