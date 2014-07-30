686e0f6bfb5b147198b96a375309856197abe45c
Nikolas Wise <nikolaswise@gmail.com>
2014-07-30 16:40:24 -0700

    neuter core

--- source/angular-dimple-core.js
+++ source/angular-dimple-core.js
@@ -2,22 +2,30 @@ angular.module('angular-dimple.core', [])
 
 .service('angular-dimple.core', [function(){
   return {
-    filter: function (chart, scopeData, field, value, filter) {
-      if (scopeData !== null) {
-        var data = this.filterData(scopeData, filter);
-        chart.data = data;
-        if (value !== undefined) {
-          chart.data = dimple.filterData(data, field, [value]);
-        }
-      }
+    filter: function (chart, scope, attrs) {
+      // chart;
+      // scope.filters
+
+      // field
+      // value
+
+
+
+      // if (scopeData !== null) {
+      //   var data = this.filterData(scopeData, filter);
+      //   chart.data = data;
+      //   if (value !== undefined) {
+      //     chart.data = dimple.filterData(data, field, [value]);
+      //   }
+      // }
     },
     filterData: function (data, filter) {
-      if (filter) {
-        var filters = filter.split(':');
-        return dimple.filterData(data, filters[0], [filters[1]]);
-      } else {
-        return data;
-      }
+      // if (filter) {
+      //   var filters = filter.split(':');
+      //   return dimple.filterData(data, filters[0], [filters[1]]);
+      // } else {
+      //   return data;
+      // }
     }
   };
 }]);
